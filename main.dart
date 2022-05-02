0a247e53716b0037ce7a77b5f032e7f48af86948
Mohammad Moradi <itismoradi@gmail.com>
2022-05-02 18:56:41 +0430

    complete display function to draw a table

--- main.dart
+++ main.dart
@@ -1,42 +1,53 @@
 //15 puzzle console game
-bool GameOver = false;
-List<int> board = [  1,  2,  3,  4,
-                     5,  6,  7,  8,
-                     9, 10, 11, 12,
-                    13, 14, 15, 16
+import 'dart:io';
+
+bool gameOver = false;
+List board = [ 
+    [ 1,  2,  3,  4],
+    [ 5,  6,  7,  8],
+    [ 9, 10, 11, 12],
+    [13, 14, 15, 16]
 ];
+
 main(){
     setup();
     
-    while (GameOver != true) {
+    while (gameOver == false) {
         display();
         input();
         logic();
     }
 }
 
-/// only call in start of game
+// only call in start of game
 void setup(){
-    print("#==== 15 puzzle ====#");
+    print('===== 15 puzzle =====');
 }
 
 void display(){
-    print("---------------------");   
-    print("| ");
-    print("---------------------");    
-    print("| ");
-    print("---------------------");    
-    print("| ");
-    print("---------------------");    
-    print("| ");
-    print("---------------------");
+    print('---------------------'); 
+
+    for (var i = 0; i < 4; i++){
+        //print a cell wall
+        stdout.write('| ');
+        //print 4 cells in any row
+        for (var j = 0; j < 4; j++) {
+            if(board[i][j] < 10){
+                stdout.write(' ');
+            }
+            stdout.write(board[i][j]);
+            stdout.write(' | ');
+        }
+
+        print('\n---------------------');
+    }
 }
 
 void input(){
-
+    
 }
 
 
 void logic(){
-    GameOver = true;
+    gameOver = true;
 }
