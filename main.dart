d38a575ab830498b1a8a2e63bb077a2d80599cca
Mohammad Moradi <itismoradi@gmail.com>
2022-05-02 11:17:50 +0430

    add 4 essential functions setup, display, input and logic functions

--- main.dart
+++ main.dart
@@ -1,3 +1,42 @@
-void main(){
-    print("15 puzzle");
+//15 puzzle console game
+bool GameOver = false;
+List<int> board = [  1,  2,  3,  4,
+                     5,  6,  7,  8,
+                     9, 10, 11, 12,
+                    13, 14, 15, 16
+];
+main(){
+    setup();
+    
+    while (GameOver != true) {
+        display();
+        input();
+        logic();
+    }
+}
+
+/// only call in start of game
+void setup(){
+    print("#==== 15 puzzle ====#");
+}
+
+void display(){
+    print("---------------------");   
+    print("| ");
+    print("---------------------");    
+    print("| ");
+    print("---------------------");    
+    print("| ");
+    print("---------------------");    
+    print("| ");
+    print("---------------------");
+}
+
+void input(){
+
+}
+
+
+void logic(){
+    GameOver = true;
 }
