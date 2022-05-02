05b4cde67b829fb0864e6d751bf6fcbe68e33aec
Mohammad Moradi <itismoradi@gmail.com>
2022-05-03 00:04:10 +0430

    add Board class and define Command enumeration complete input function

--- main.dart
+++ main.dart
@@ -2,20 +2,33 @@
 import 'dart:io';
 
 bool gameOver = false;
-List board = [ 
-    [ 1,  2,  3,  4],
-    [ 5,  6,  7,  8],
-    [ 9, 10, 11, 12],
-    [13, 14, 15, 16]
-];
+
+class Board{
+    List table = [ 
+        [ 1,  2,  3,  4],
+        [ 5,  6,  7,  8],
+        [ 9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ];
+
+    Command command = Command.UP;
+}
+
+enum Command {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+}
 
 main(){
     setup();
-    
+    Board board = new Board();
+
     while (gameOver == false) {
-        display();
-        input();
-        logic();
+        display(board);
+        board.command = input();
+        logic(board);
     }
 }
 
@@ -24,7 +37,7 @@ void setup(){
     print('===== 15 puzzle =====');
 }
 
-void display(){
+void display(Board board){
     print('---------------------'); 
 
     for (var i = 0; i < 4; i++){
@@ -32,10 +45,10 @@ void display(){
         stdout.write('| ');
         //print 4 cells in any row
         for (var j = 0; j < 4; j++) {
-            if(board[i][j] < 10){
+            if(board.table[i][j] < 10){
                 stdout.write(' ');
             }
-            stdout.write(board[i][j]);
+            stdout.write(board.table[i][j]);
             stdout.write(' | ');
         }
 
@@ -43,11 +56,26 @@ void display(){
     }
 }
 
-void input(){
-    
+Command input(){
+    String? key = stdin.readLineSync();
+    Command command = Command.UP;
+
+    if(key == 'w' || key == 'W'){
+        command = Command.UP;
+    }
+    else if(key == 's' || key == 'S'){
+        command = Command.DOWN;
+    }
+    else if(key == 'd' || key == 'D'){
+        command = Command.RIGHT;
+    }
+    else if(key == 'a' || key == 'A'){
+        command = Command.LEFT;
+    }
+
+    return command;
 }
 
-
-void logic(){
+void logic(Board board){
     gameOver = true;
 }
