c94b2161ba6eb61ff23e38e09bbaff62bc61bec3
Hessam <hessam2koucheki@gmail.com>
2022-05-08 17:48:21 +0430

    add shuffle function

--- python3/main.py
+++ python3/main.py
@@ -1,29 +1,52 @@
 
 
+from time import sleep
+
+
 class Board:
     def __init__(self):
         self.board = [[1, 2, 3, 4],
-                 [5, 6, 7, 8],
-                 [9, 10, 11, 12],
-                 [13, 14, 15, '  ']]
+                      [5, 6, 7, 8],
+                      [9, 10, 11, 12],
+                      [13, 14, 15, '  ']]
 
+    def swap_up(self, x, y):
+        if x > 0 and y > 0:
+            self.board[x][y], self.board[x - 1][y] = self.board[x - 1][y], self.board[x][y]
+        return self.board
+    
+    def swap_down(self, x, y):
+        if y < 3:
+            self.swap_up(x, y+1)
+        
+    def swap_left(self, x, y):
+        self.board[x][y], self.board[x][y - 1] = self.board[x][y - 1], self.board[x][y]
+        return self.board
+    
+    def swap_right(self, x, y):
+        if x < 3:
+            self.swap_left(x+1, y)
+        
     def shuffle(self):
-        1
-
-    def swap(self, x, y, z, w):
-        self.board[x][y], self.board[z][w] = self.board[z][w], self.board[x][y]
+        from random import randint, choice
+        for i in range(100):
+            func = choice([self.swap_up, self.swap_down, self.swap_left, self.swap_right])
+            func(randint(0, 3), randint(0, 3))
         return self.board
     
     def display(self):
-        length_of_table = 21 
+        length_of_table = 21
         from os import system
         system(command='cls')
         for i in range(4):
             print('-' * length_of_table)
             for j in range(4):
-                print(f'|  {self.board[i][j]} ' if (self.board[i][j] is not '  ' and self.board[i][j] < 10) else f'| {self.board[i][j]} ', end='')
+                print(f'|  {self.board[i][j]} ' if (
+                    self.board[i][j] is not '  ' and self.board[i][j] < 10) else f'| {self.board[i][j]} ', end='')
             print('|')
         print('-' * length_of_table)
 
+
 b = Board()
+b.shuffle()
 b.display()
