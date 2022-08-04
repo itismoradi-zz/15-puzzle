dc4681d43b525e7df744fcda3dfe7741ddc015b0
Mohammad Moradi <itismoradi@gmail.com>
2022-08-04 16:26:06 +0430

    Add ui buttons clicked slots
    Change ui buttons names
    Add a function to find buttons by position
    Add logic function

--- Qt/mainwindow.cpp
+++ Qt/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <exception>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +15,158 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_btn0_0_clicked()
+{
+    logic(0, 0);
+}
+
+
+void MainWindow::on_btn1_0_clicked()
+{
+    logic(1, 0);
+}
+
+
+void MainWindow::on_btn2_0_clicked()
+{
+    logic(2, 0);
+}
+
+
+void MainWindow::on_btn3_0_clicked()
+{
+    logic(3, 0);
+}
+
+
+void MainWindow::on_btn0_1_clicked()
+{
+    logic(0, 1);
+}
+
+
+void MainWindow::on_btn1_1_clicked()
+{
+    logic(1, 1);
+}
+
+
+void MainWindow::on_btn2_1_clicked()
+{
+    logic(2, 1);
+}
+
+
+void MainWindow::on_btn3_1_clicked()
+{
+    logic(3, 1);
+}
+
+
+void MainWindow::on_btn0_2_clicked()
+{
+    logic(0, 2);
+}
+
+
+void MainWindow::on_btn1_2_clicked()
+{
+    logic(1, 2);
+}
+
+
+void MainWindow::on_btn2_2_clicked()
+{
+    logic(2, 2);
+}
+
+
+void MainWindow::on_btn3_2_clicked()
+{
+   logic(3, 2);
+}
+
+
+void MainWindow::on_btn0_3_clicked()
+{
+    logic(0, 3);
+}
+
+
+void MainWindow::on_btn1_3_clicked()
+{
+    logic(1, 3);
+}
+
+
+void MainWindow::on_btn2_3_clicked()
+{
+    logic(2, 3);
+}
+
+
+void MainWindow::on_btn3_3_clicked()
+{
+    logic(3, 3);
+}
+
+void MainWindow::logic(usInt x, usInt y)
+{
+    Cell & empty = ~board;
+    Cell & target = board(x, y);
+
+    if(empty.x == target.x || empty.y == target.y)
+    {
+        if(empty.y == target.y - 1 || empty.y == target.y + 1 ||
+           empty.x == target.x - 1 || empty.x == target.x + 1)
+        {
+            board.swapCells(empty.x, empty.y, target.x, target.y);
+            findButton(empty.x, empty.y)->setText(QString::number(empty.value));
+            findButton(x, y)->setText("");
+            ui->lbl_status->setText("😎👍");
+            return;
+        }
+    }
+
+    ui->lbl_status->setText("😒 Can't move!");
+}
+
+QPushButton *MainWindow::findButton(usInt x, usInt y)
+{
+    if(x == 0)
+    {
+        if(y == 0)  return ui->btn0_0;
+        if(y == 1)  return ui->btn0_1;
+        if(y == 2)  return ui->btn0_2;
+        if(y == 3)  return ui->btn0_3;
+    }
+    else if(x == 1)
+    {
+        if(y == 0)  return ui->btn1_0;
+        if(y == 1)  return ui->btn1_1;
+        if(y == 2)  return ui->btn1_2;
+        if(y == 3)  return ui->btn1_3;
+    }
+    else if(x == 2)
+    {
+        if(y == 0)  return ui->btn2_0;
+        if(y == 1)  return ui->btn2_1;
+        if(y == 2)  return ui->btn2_2;
+        if(y == 3)  return ui->btn2_3;
+    }
+    else if(x == 3)
+    {
+        if(y == 0)  return ui->btn3_0;
+        if(y == 1)  return ui->btn3_1;
+        if(y == 2)  return ui->btn3_2;
+        if(y == 3)  return ui->btn3_3;
+    }
+    else
+    {
+        throw invalid_argument("Invalid position is entered!");
+    }
 }
 
--- Qt/mainwindow.hpp
+++ Qt/mainwindow.hpp
@@ -2,10 +2,13 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+#include "board.hpp"
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +18,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    //click on board buttons
+    void on_btn0_0_clicked();
+    void on_btn1_0_clicked();
+    void on_btn2_0_clicked();
+    void on_btn3_0_clicked();
+    void on_btn0_1_clicked();
+    void on_btn1_1_clicked();
+    void on_btn2_1_clicked();
+    void on_btn3_1_clicked();
+    void on_btn0_2_clicked();
+    void on_btn1_2_clicked();
+    void on_btn2_2_clicked();
+    void on_btn3_2_clicked();
+    void on_btn0_3_clicked();
+    void on_btn1_3_clicked();
+    void on_btn2_3_clicked();
+    void on_btn3_3_clicked();
+
 private:
     Ui::MainWindow *ui;
+    Board board;
+    void logic(usInt, usInt);
+    QPushButton * findButton(usInt, usInt);     //return ui button by position
 };
 #endif // MAINWINDOW_HPP
